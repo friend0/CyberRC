d8bbfa3d870a61fa8054174ea45311243a1ca762
Ryan A. Rodriguez <ryan.and.rodriguez@gmail.com>
2024-09-15 19:13:14 -0700

    add initial embedded code for gamepad interface

---
+++ TeensyCyberRC/src/main.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include <nanopbSerial.h>
+#include <pb.h>
+#include "RCData.pb.h"
+
+cyberrc_RCData rc_message = cyberrc_RCData_init_zero;
+Stream& proto = Serial;
+Stream& debug = SerialUSB1;
+
+void setup() {
+    // Start the serial communication
+    // Serial.begin(115200);
+
+    // Initialize the Joystick emulation
+    Joystick.begin();
+    bool val = 0;
+}
+
+void loop() {
+    static bool val = 0;
+    // Check if serial data is available
+    bool proto_decode_status;
+    if (Serial.available() > 0) { 
+
+        pb_istream_s pb_in;
+        pb_in = pb_istream_from_serial(proto, 36);
+        proto_decode_status = pb_decode(&pb_in, cyberrc_RCData_fields, &rc_message);
+        if (!proto_decode_status) {
+            debug.println("Decoding failed");
+            return;
+        }
+
+
+
+
+        // You can extend this logic for more buttons/axes based on serial input
+    } else {
+        Joystick.X(1);
+        Joystick.button(1, val);
+        val = !val;
+    }
+
+    delay(250);  // Small delay for stability
+}
