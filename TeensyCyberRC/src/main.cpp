094fcd3d4a3e6bcbb97e6a11706bd6316f20322a
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2024-10-11 01:01:05 -0700

    basic handling of control proto input

--- TeensyCyberRC/src/main.cpp
+++ TeensyCyberRC/src/main.cpp
@@ -4,6 +4,7 @@
 #include "RCData.pb.h"
 
 cyberrc_RCData rc_message = cyberrc_RCData_init_zero;
+cyberrc_RCData last_rc_message = cyberrc_RCData_init_zero;
 Stream& proto = Serial;
 
 void setup() {
@@ -23,9 +24,12 @@ void loop() {
 
         pb_istream_s pb_in;
         pb_in = pb_istream_from_serial(proto, 36);
-        proto_decode_status = pb_decode(&pb_in, cyberrc_RCData_fields, &rc_message);
-        if (!proto_decode_status) {
-            return;
+        proto_decode_status = pb_decode(&pb_in, cyberrc_RCData_fields, &c_message);
+        if (proto_decode_status) {
+            message_handler(rc_message, last_rc_message);
+            last_rc_message = rc_message;
+        } else {
+          Serial.println("Failed to read message");
         }
         // You can extend this logic for more buttons/axes based on serial input
     } else {
@@ -35,4 +39,21 @@ void loop() {
     }
 
     delay(250);  // Small delay for stability
+}
+
+void messaage_handler(cyberrc_RCData &rc_message, cyberrc_RCData &last_rc_message)
+    // Handle the message here
+    if (rc_message.throttle != last_rc_message.throttle) {
+      Joystick.Y(rc_message.throttle);
+    }
+    if (rc_message.roll != last_rc_message.roll) {
+      Joystick.RotateZ(rc_message.roll);
+    }
+    if (rc_message.pitch != last_rc_message.pitch) {
+      Joystick.Z(rc_message.pitch);
+    }
+    if (rc_message.yaw != last_rc_message.yaw) {
+      Joystick.X(rc_message.yaw);
+    }
+    // TODO: figure out the correct maping for arm and mode buttons
 }
