9690f7fdc8ad2e00c6127dcc4b147d53d34a503d
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2024-10-20 01:13:15 -0700

    ethernet code

--- TeensyCyberRC/src/main.cpp
+++ TeensyCyberRC/src/main.cpp
@@ -1,41 +1,25 @@
-/*
- *  Project     Arduino XInput Library
- *  @author     David Madison
- *  @link       github.com/dmadison/ArduinoXInput
- *  @license    MIT - Copyright (c) 2019 David Madison
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- *  Example:      SimulateAll
- *  Description:  Automatically activate all possible XInput controls.
- *                Useful to test that everything is functioning properly.
- * 
- *  WARNING: This will spam inputs! Ground pin '0' (RX) to stop.
- *
- */
-
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 #include <XInput.h>
+
+#include <nanopbSerial.h>
+#include <pb.h>
+#include "RCData.pb.h"
+
+cyberrc_RCData rc_message = cyberrc_RCData_init_zero;
+cyberrc_RCData last_rc_message = cyberrc_RCData_init_zero;
+Stream& proto = Udp;
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = {
+  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
+};
+
 // Config Settings
 const unsigned long CycleTime = 5000;  // ms
 const int SafetyPin = 0;  // Ground this pin to prevent inputs
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
 
 // Button Setup
 const int NumButtons = 10;
@@ -51,6 +35,7 @@ const int Buttons[NumButtons] = {
 	BUTTON_L3,
 	BUTTON_R3,
 };
+
 const unsigned long ButtonDuration = CycleTime / NumButtons;
 unsigned long buttonTimeLast = 0;
 int buttonID = 0;
@@ -60,6 +45,11 @@ const int NumDirections = 4;
 const unsigned long DPadDuration = CycleTime / NumDirections;
 unsigned long dpadTimeLast = 0;
 int dpadPosition = 0;
+
+// Ethernet Setup
+EthernetUDP Udp;
+IPAddress ip;
+unsigned int localPort = 6969;
 
 // Triggers
 const int TriggerMax = 255;  // uint8_t max
@@ -73,13 +63,56 @@ const int JoyMax = 32767;  // int16_t max
 const double angle_precision = (2 * PI) / (CycleTime / 4);  // 4 because 250 Hz update rate
 double angle = 0.0;
 
+const int ledPin = 13;
+
+void blink_loop(int pin, int delay_ms) {
+    bool ledState = false;
+    while (true) {
+        digitalWrite(pin, ledState);
+        delay(delay_ms);
+    }
+}
+
 void setup() {
 	pinMode(SafetyPin, INPUT_PULLUP);
 	XInput.setAutoSend(false);  // Wait for all controls before sending
 	XInput.begin();
+    // TODO: replace delays with a blink loop
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+        blink_loop(ledPin, 1000);
+    }
+    if (Ethernet.linkStatus() == LinkOFF) {
+        blink_loop(ledPin, 3000);
+    }
+    if (Ethernet.begin(mac) == 0) {
+        // TODO: blink led
+        blink_loop(ledPin, 5000);
+    }
+    ip = Ethernet.localIP();
+    Ethernet.begin(mac, ip);
+    Udp.begin(ip);
 }
 
 void loop() {
+    bool proto_decode_status;
+
+    int packetSize = Udp.parsePacket();
+    if (packetSize) {
+        // read the packet into packetBufffer
+        char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+        Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+        // send a reply to the IP address and port that sent us the packet we received
+        Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+        Udp.write("ack");
+        Udp.endPacket();
+        pb_istream_s pb_in;
+        pb_in = pb_istream_from_serial(proto, 36);
+        proto_decode_status = pb_decode(&pb_in, cyberrc_RCData_fields, &rc_message);
+        if (!proto_decode_status) {
+            blink_loop(ledPin, 250);
+        }
+    }
+
 	if (digitalRead(SafetyPin) == LOW) {
 		return;
 	}
@@ -106,22 +139,6 @@ void loop() {
 		if (buttonID >= NumButtons) buttonID = 0;  // Go back to 0 if we hit the limit
 
 		buttonTimeLast = t;  // Save time we last did this
-	}
-
-	// Triggers
-	if (t - triggerTimeLast >= TriggerDuration) {  // If enough time has passed, update the triggers
-		XInput.setTrigger(TRIGGER_LEFT, triggerVal);
-		XInput.setTrigger(TRIGGER_RIGHT, TriggerMax - triggerVal);  // Inverse
-
-		// Increment trigger value based on direction
-		if (triggerDirection == 0) { triggerVal++; }
-		else { triggerVal--; }
-
-		if (triggerVal == TriggerMax || triggerVal == 0) {  // If we've hit the edge of the range
-			triggerDirection = !triggerDirection;  // Reverse direction
-		}
-
-		triggerTimeLast = t;  // Save time we last did this
 	}
 
 	// Calculate joystick x/y values using trig
