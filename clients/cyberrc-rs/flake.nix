561fdf558ee30d6e4b8811bec260ba6c114e9d72
Ryan Rodriguez <ryan.and.rodriguez@gmail.com>
2024-11-11 19:01:55 -0800

    wip

--- clients/cyberrc-rs/flake.nix
+++ clients/cyberrc-rs/flake.nix
@@ -1,22 +1,46 @@
 {
+  description = "Rust development environment with Cargo dependencies";
+
+  # Specify the inputs, such as nixpkgs
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
-      }
-    );
+  # Define the outputs
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    # Define a devShell for development
+    devShell = pkgs.mkShell {
+      # Add Rust and Cargo to the environment
+      buildInputs = [
+        pkgs.protobuf
+        pkgs.cmake
+        pkgs.rustup
+        pkgs.zsh
+      ];
+
+      # Optionally, set environment variables
+      CARGO_HOME = "./.cargo";
+      RUST_BACKTRACE = "1"; # Enable backtrace for debugging
+
+      # Optional shellHook to fetch dependencies when entering the shell
+x      shellHook = ''
+        export GIT_CONFIG=$PWD/.gitconfig
+        export CARGO_NET_GIT_FETCH_WITH_CLI=true
+        export GIT_SSH_COMMAND="ssh -F ~/.ssh/config"  # Ensure it uses your SSH config
+        # Start Zsh if not already the active shell
+        if [ "$SHELL" != "$(command -v zsh)" ]; then
+          export SHELL="$(command -v zsh)"
+          exec zsh
+        fi
+        echo "Entering Rust development environment..."
+        rustup default nightly
+        cargo install rerun-cli
+        cargo fetch # Pre-fetch dependencies defined in Cargo.toml
+      '';
+    };
+  });
 }
+
