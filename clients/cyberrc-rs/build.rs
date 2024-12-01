04939d0dfc2457f72d06e4d676f6a80021b66903
Ryan Rodriguez <ryan.and.rodriguez@gmail.com>
2024-11-30 16:50:07 -0800

    buidl script points to protos in firmware

--- clients/cyberrc-rs/build.rs
+++ clients/cyberrc-rs/build.rs
@@ -2,8 +2,8 @@ use prost_build;
 use std::io::Result;
 
 fn main() -> Result<()> {
-    let proto_files = &["../../protos/RCData.proto"];
-    let proto_include = &["../../protos"];
+    let proto_files = &["../../firmware/src/protos/RCData.proto"];
+    let proto_include = &["../../firmware/src/protos"];
 
     prost_build::compile_protos(proto_files, proto_include).expect("Failed to compile protos");
     Ok(())
